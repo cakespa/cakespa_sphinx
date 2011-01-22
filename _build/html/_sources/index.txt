3bd5db53c559c4d5623d27fcfa6d53bea1facb57
tfmagician <tfmagician@gmail.com>
2011-01-22 19:30:08 +0900

    Creating _themes/cakespa.

--- _build/html/_sources/index.txt
+++ _build/html/_sources/index.txt
@@ -1,20 +1,3 @@
-.. cakespa documentation master file, created by
-   sphinx-quickstart on Sat Jan 22 15:35:14 2011.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to cakespa's documentation!
-===================================
-
-Contents:
-
-.. toctree::
-   :maxdepth: 2
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+ようこそ!
+=========
 
--- _build/html/searchindex.js
+++ _build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,cakespa:0,modul:0,indic:0,content:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to cakespa&#8217;s documentation!"],objnames:{},filenames:["index"]})
+Search.setIndex({objects:{},terms:{index:[],search:[],welcom:[],cakespa:[],modul:[],"\u3088\u3046\u3053\u305d":0,indic:[],content:[],tabl:[],document:[],page:[]},objtypes:{},titles:["\u3088\u3046\u3053\u305d!"],objnames:{},filenames:["index"]})
